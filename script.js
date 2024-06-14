9a0830108ab919f3831cb496a8c89dcdb78ca11d
Archana G <archanadevig10@gmail.com>
2024-06-14 12:49:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Find the element with the id 'year' and set its content to the current year
+document.getElementById("year").textContent = currentYear;
+
+// Script for navigation bar
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+//sproduct
+var mainImg = document.getElementById("mainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function () {
+  mainImg.src = smallImg[0].src;
+};
+
+smallImg[1].onclick = function () {
+  mainImg.src = smallImg[1].src;
+};
+
+smallImg[2].onclick = function () {
+  mainImg.src = smallImg[2].src;
+};
+
+smallImg[3].onclick = function () {
+  mainImg.src = smallImg[3].src;
+};
